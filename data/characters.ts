27581ae937f1e0b6dfdcd0c0c54b387caf71d8d6
João Pedro <jaaum.p95@gmail.com>
2025-05-27 23:48:15 -0300

    chore: add leon as character

--- data/characters.ts
+++ data/characters.ts
@@ -19,4 +19,10 @@ export const characterData: Record<string, CharacterInfo> = {
       "Eiselin Burchelli Meijal Hulkenberg, known simply as Hulkenberg, is a major playable character in Metaphor: ReFantazio. She is a knight of the populous roussainte tribe and a former member of the royal family's Kingsguard. After failing to protect the prince during an assassination attempt, she wandered the kingdom in shame. She returns to combat upon hearing news of King Hythlodaeus V's death.",
     title: "Aizerin Barucherri Maiaru Hyurukenberugu",
   },
+  "Leon Strohl": {
+    src: "/leon.webp",
+    description:
+      "Leon Strohl da Haliaetus, known simply as Strohl, is a major playable character in Metaphor: ReFantazio. He is of the populous clemar tribe and the son of the fallen noble family of Halia.",
+    title: "Reon Sutorouru Da Harieitasu",
+  }
 };
